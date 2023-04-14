ec24c25a6fccd3b68d42c706db5007f6ca6742d7
Subrato Mondal <subrato.mondal18@gmail.com>
2023-04-14 08:23:00 +0530

    Regression Model Implemented

--- src/components/ChartSection.js
+++ src/components/ChartSection.js
@@ -1,15 +1,42 @@
 import React, {useState} from "react";
 import { useSelector } from "react-redux";
-import { Scatter } from "react-chartjs-2";
-import { LinearScale, Chart, PointElement } from "chart.js";
+import { Chart } from "react-chartjs-2";
+import { Chart as ChartJS, 
+  LinearScale, 
+  CategoryScale, 
+  BarElement, 
+  PointElement, 
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController, 
+  Scatter} from "chart.js";
 
 const ChartSection = (prop) => {
 
     const currFileName = useSelector(state => state.currentFileState.fileName);
     const currFileDataSet = useSelector(state => state.currentFileState.dataSet);
+    const currFileFitCurve = useSelector(state => state.currentFileState.fitCurve);
+    const coef = useSelector(state => state.currentFileState.coef);
+    const intercept = useSelector(state => state.currentFileState.intercept);
 
-    Chart.register(LinearScale);
-    Chart.register(PointElement);
+    let chartTitle = currFileName;
+    
+    if(coef>0 || intercept>0)
+      chartTitle = chartTitle + " [ intercept = " + intercept + ", coef = " +coef+"]";
+
+      ChartJS.register(
+        LinearScale,
+        CategoryScale,
+        BarElement,
+        PointElement,
+        LineElement,
+        Legend,
+        Tooltip,
+        LineController,
+        BarController
+      );
 
     const options = {
         scales: {
@@ -19,27 +46,39 @@ const ChartSection = (prop) => {
         }
     };
 
-    console.log('At Chart : '+JSON.stringify(currFileDataSet));
+    //console.log('At Chart : '+JSON.stringify(currFileDataSet));
+    //console.log('FitData : '+JSON.stringify(currFileFitCurve));
 
+    const lineType = 'line';
+    const scatterType = 'scatter';
     const data = {
         datasets: [
           {
-            label: 'A dataset',
+            type: scatterType,
+            label: 'Dataset',
             data: currFileDataSet,
             backgroundColor: 'rgba(255, 99, 132, 1)',
+          },
+          {
+            type: lineType,
+            label: 'Fit Curve',
+            data: currFileFitCurve,
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgb(75, 192, 192)',
+            borderWidth: 2
           }
         ]
       };
 
     return (
-        <div class="card">
-            <div class="card-header">
-                {currFileName}
-            </div>
-            <div class="card-body">
-                <Scatter data={data} options={options}/>
-            </div>
-        </div>
+          <div class="card">
+              <div class="card-header">
+                  {chartTitle}
+              </div>
+              <div class="card-body">
+                  <Chart type={lineType} data={data} options={options}/>
+              </div>
+          </div>
     );
 }
 
--- src/reduxStores/CurrentFileSlice.js
+++ src/reduxStores/CurrentFileSlice.js
@@ -2,7 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     fileName: "Select File Name",
-    dataSet: [{x: 10,y: 5.55}]
+    dataSet: [{x: 10,y: 5.55}],
+    intercept: 0,
+    coef: 0,
+    fitCurve: [{x: 10,y: 5.55}]
 };
 
 export const CurrentFileSlice = createSlice({
@@ -12,7 +15,10 @@ export const CurrentFileSlice = createSlice({
         updateFileState: (state, action) => {
             state.fileName = action.payload.fileName;
             state.dataSet = action.payload.dataSet;
-            console.log(JSON.stringify(state));
+            state.intercept = action.payload.intercept;
+            state.coef = action.payload.coef;
+            state.fitCurve = action.payload.fitCurve;
+            //console.log(JSON.stringify(state));
         }
     }
 });
